a4b308ec3e56a3a9b2b2a4762280865a70f15f59
Hamish <hamishfpso@gmail.com>
2017-09-29 11:42:08 +0100

    second test passed

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,4 +1,12 @@
+# def pet_shop_name(pet_shop)
+#   name = pet_shop[:name]
+#   return name
+# end
+
 def pet_shop_name(pet_shop)
-  name = pet_shop[:name]
-  return name
+  return pet_shop[:name]
+end
+
+def total_cash(money)
+  return money[:admin][:total_cash]
 end
